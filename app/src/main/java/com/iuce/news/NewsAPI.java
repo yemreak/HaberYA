e348196f892b82492373eec162946d907c8c4b29
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-12-19 01:02:57 +0300

    😏 Farklı API keyler ile istek gönderilmekte

--- app/src/main/java/com/iuce/news/NewsAPI.java
+++ app/src/main/java/com/iuce/news/NewsAPI.java
@@ -13,22 +13,27 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 class NewsAPI {
     static final String TAG = "NewsAPI"; // May be in `strings.xml`
 
-    private static final String API_KEY = "cf9168e3e5ff4e8987492262f92632fb"; // Not secure!
-    private static final String MAIN_URL = "https://newsapi.org/v2/top-headlines?country=tr&apiKey=" + API_KEY;
+    private static final String[] API_KEYS = {"cf9168e3e5ff4e8987492262f92632fb", "f82c72913e944b0c838e24a52e90db8c"}; // Not secure!
+    private static final String URL_TEMPLATE = "https://newsapi.org/v2/top-headlines?country=tr&apiKey=%s";
+
+    private static String generateURL() {
+        int ix = new Random().nextInt(API_KEYS.length);
+        return String.format(URL_TEMPLATE, API_KEYS[ix]);
+    }
 
     /**
-     * https://developer.android.com/training/volley/simple.html
-     *
-     * @return
+     * Details: https://android.yemreak.com/temel/http-istekleri
+     * Reference: https://developer.android.com/training/volley/simple.html
      */
     static void requestNewsData(Context context, ResponseListener responseListener) {
 
         RequestQueue queue = Volley.newRequestQueue(context);
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, MAIN_URL, (response) -> {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, generateURL(), (response) -> {
             // https://stackoverflow.com/a/9606629/9770490
             try {
                 // new JSONObject(response).getJSONArray("articles").get(1).getString("source")
