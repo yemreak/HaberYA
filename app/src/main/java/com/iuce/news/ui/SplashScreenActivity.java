0165bb5f42b5f6e1259810dec0e9b15a91d4bf3b
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2020-01-04 18:33:59 +0300

    👨‍🔧 Yazım hatası giderildi

--- app/src/main/java/com/iuce/news/ui/SplashScreenActivity.java
+++ app/src/main/java/com/iuce/news/ui/SplashScreenActivity.java
@@ -1,23 +1,24 @@
 package com.iuce.news.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ProgressBar;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.iuce.news.R;
 
 
 /*
-* Example: https://github.com/SabithPkcMnr/SplashScreen
-*/
-public class SpalshScreenActivity extends AppCompatActivity {
+ * Example: https://github.com/SabithPkcMnr/SplashScreen
+ */
+public class SplashScreenActivity extends AppCompatActivity {
 
     ProgressBar splashProgress;
-    int SPLASH_TIME = 2000;
+
+    final int SPLASH_TIME = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +28,19 @@ public class SpalshScreenActivity extends AppCompatActivity {
         splashProgress = findViewById(R.id.splashProgress);
         playProgress();
 
-        new Handler().postDelayed(()-> {
-                Intent mySuperIntent = new Intent(SpalshScreenActivity.this, MainActivity.class);
-                startActivity(mySuperIntent);
+        new Handler().postDelayed(() -> {
+            Intent mySuperIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(mySuperIntent);
 
-                // remove activity from activity stack
-                finish();
+            // remove activity from activity stack
+            finish();
 
         }, SPLASH_TIME);
     }
 
     private void playProgress() {
         ObjectAnimator.ofInt(splashProgress, "progress", 100)
-                .setDuration(3000)
+                .setDuration(SPLASH_TIME)
                 .start();
     }
 }
