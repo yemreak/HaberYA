303f21dd945317d66f6d3b4f6a5f15f28ca1e83f
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-01-15 23:19:10 +0300

    🤹‍♀️ Arama fonksiyonelliği tamamlandı

--- app/src/main/java/com/iuce/news/ui/SearchActivity.java
+++ app/src/main/java/com/iuce/news/ui/SearchActivity.java
@@ -1,18 +1,61 @@
 package com.iuce.news.ui;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.iuce.news.R;
+import com.iuce.news.api.NewsAPI;
+import com.iuce.news.api.NewsAPIOptions;
+import com.iuce.news.db.entity.News;
+
+import java.util.List;
+import java.util.Objects;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private static String query;
+    private LinearLayoutManager linearLayoutManager;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        String query = getIntent().getStringExtra("QUERY");
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView = findViewById(R.id.search_recycler_view);
 
+        query = getIntent().getStringExtra("QUERY");
+        getRequestedNews();
+    }
+
+    private void getRequestedNews() {
+        NewsAPIOptions options = NewsAPIOptions.Builder().setQuery(query).build();
+        if (isConnected()) {
+            NewsAPI.requestTopHeadlines(this, this::initRecyclerView, options);
+        }
+    }
+
+    private void initRecyclerView(List<News> newsList) {
+        SearchAdapter searchAdapter = new SearchAdapter(this, newsList);
+        recyclerView.setAdapter(searchAdapter);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    private boolean isConnected() {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = Objects.requireNonNull(connMgr).getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        boolean isWifiConn = Objects.requireNonNull(networkInfo).isConnected();
+
+        networkInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        boolean isMobileConn = Objects.requireNonNull(networkInfo).isConnected();
+
+        return isWifiConn || isMobileConn;
     }
 }
