89ab599add63fa44446e1290131d1439aeda7ed3
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-01-19 16:27:21 +0300

    🧹 Gereksiz menü kaldırıldı

--- app/src/main/java/com/iuce/news/ui/ReactedActivity.java
+++ app/src/main/java/com/iuce/news/ui/ReactedActivity.java
@@ -1,8 +1,6 @@
 package com.iuce.news.ui;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -84,39 +82,6 @@ public class ReactedActivity extends AppCompatActivity {
         reactedRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        /*
-         * Details:
-         * https://google-developer-training.github.io/android-developer-fundamentals-course-concepts-v2/unit-2-user-experience/lesson-4-user-interaction/4-3-c-menus-and-pickers/4-3-c-menus-and-pickers.html
-         */
-        getMenuInflater().inflate(R.menu.main_layout_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.get_all_reacted_but:
-                switchContent(null);
-                return true;
-            case R.id.get_liked_but:
-                switchContent(State.Type.LIKED);
-                return true;
-            case R.id.get_saved_but:
-                switchContent(State.Type.LATER);
-                return true;
-            case android.R.id.home:
-                finish();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void switchContent(State.Type type) {
-        TYPE = type;
-        initRecyclerView();
-    }
 
     private void setBackButton() {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
