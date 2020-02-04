2df141f36b3ab2371ca85c2c72fe625f655ad30f
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2020-02-05 01:36:42 +0300

    👨‍💻 Kod formatlandı (#43)

--- app/src/main/java/com/yemreak/haberya/api/newsapi/THOptions.java
+++ app/src/main/java/com/yemreak/haberya/api/newsapi/THOptions.java
@@ -10,118 +10,118 @@ import androidx.annotation.NonNull;
  */
 public class THOptions extends Options {
 
-    public static final String HEAD = "top-headlines";
+	public static final String HEAD = "top-headlines";
 
-    private Country country;
-    private Category category;
+	private Country country;
+	private Category category;
 
-    private String sources;
-    private String query;
+	private String sources;
+	private String query;
 
-    private int pageSize;
-    private int page;
+	private int pageSize;
+	private int page;
 
-    THOptions(Builder builder) {
-        this.country = builder.country;
-        this.category = builder.category;
-        this.sources = builder.sources;
-        this.query = builder.query;
-        this.pageSize = builder.pageSize;
-        this.page = builder.page;
-    }
+	THOptions(Builder builder) {
+		this.country = builder.country;
+		this.category = builder.category;
+		this.sources = builder.sources;
+		this.query = builder.query;
+		this.pageSize = builder.pageSize;
+		this.page = builder.page;
+	}
 
-    public static Builder Builder() {
-        return new Builder();
-    }
+	public static Builder Builder() {
+		return new Builder();
+	}
 
-    public String buildUrl(String apiKey) {
-        return super.buildUrl(HEAD, buildQueries(), apiKey);
-    }
+	public String buildUrl(String apiKey) {
+		return super.buildUrl(HEAD, buildQueries(), apiKey);
+	}
 
-    private String buildQueries() {
-        return trimQuery("" // 🦅
-                + generateQuery("country", country)
-                + generateQuery("category", category)
-                + generateQuery("sources", sources)
-                + generateQuery("q", query)
-                + generateQuery("pageSize", pageSize)
-                + generateQuery("page", page)
-        );
-    }
+	private String buildQueries() {
+		return trimQuery("" // 🦅
+				+ generateQuery("country", country)
+				+ generateQuery("category", category)
+				+ generateQuery("sources", sources)
+				+ generateQuery("q", query)
+				+ generateQuery("pageSize", pageSize)
+				+ generateQuery("page", page)
+		);
+	}
 
-    public Country getCountry() {
-        return country;
-    }
+	public Country getCountry() {
+		return country;
+	}
 
-    public Category getCategory() {
-        return category;
-    }
+	public Category getCategory() {
+		return category;
+	}
 
-    public String getSources() {
-        return sources;
-    }
+	public String getSources() {
+		return sources;
+	}
 
-    public String getQuery() {
-        return query;
-    }
+	public String getQuery() {
+		return query;
+	}
 
-    public int getPageSize() {
-        return pageSize;
-    }
+	public int getPageSize() {
+		return pageSize;
+	}
 
-    public int getPage() {
-        return page;
-    }
+	public int getPage() {
+		return page;
+	}
 
-    public static final class Builder {
+	public static final class Builder {
 
-        private static Country defaultCountry;
+		private static Country defaultCountry;
 
-        private Country country = defaultCountry;
-        private Category category;
+		private Country country = defaultCountry;
+		private Category category;
 
-        private String sources;
-        private String query;
+		private String sources;
+		private String query;
 
-        private int pageSize;
-        private int page;
+		private int pageSize;
+		private int page;
 
-        public static void setDefaultCountry(@NonNull Country country) {
-            defaultCountry = country;
-        }
+		public static void setDefaultCountry(@NonNull Country country) {
+			defaultCountry = country;
+		}
 
-        public Builder setCountry(Country country) {
-            this.country = country;
-            return this;
-        }
+		public Builder setCountry(Country country) {
+			this.country = country;
+			return this;
+		}
 
-        public Builder setCategory(Category category) {
-            this.category = category;
-            return this;
-        }
+		public Builder setCategory(Category category) {
+			this.category = category;
+			return this;
+		}
 
-        public Builder setSources(String sources) {
-            this.sources = sources;
-            return this;
-        }
+		public Builder setSources(String sources) {
+			this.sources = sources;
+			return this;
+		}
 
-        public Builder setQuery(String query) {
-            this.query = query;
-            return this;
-        }
+		public Builder setQuery(String query) {
+			this.query = query;
+			return this;
+		}
 
-        public Builder setPageSize(int pageSize) {
-            this.pageSize = pageSize;
-            return this;
-        }
+		public Builder setPageSize(int pageSize) {
+			this.pageSize = pageSize;
+			return this;
+		}
 
-        public Builder setPage(int page) {
-            this.page = page;
-            return this;
-        }
+		public Builder setPage(int page) {
+			this.page = page;
+			return this;
+		}
 
-        public THOptions build() {
-            return new THOptions(this);
-        }
-    }
+		public THOptions build() {
+			return new THOptions(this);
+		}
+	}
 }
